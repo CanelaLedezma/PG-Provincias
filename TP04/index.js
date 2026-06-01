27dc78077f90b3aaaecbe7364ef35fbfa084fddd
Android Studio <devandroid@ort>
2026-06-01 08:59:41 -0300

    index arreglado

--- TP04/index.js
+++ TP04/index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import ProvinceService from './src/services/province-service.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const service = new ProvinceService();
 
-app.get('/', (req, res) => {
-    res.send('API funcionando');
+app.get('/api/province', async (req, res) => {
+    const provinces = await service.getAllAsync();
+    res.json(provinces);
 });
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
