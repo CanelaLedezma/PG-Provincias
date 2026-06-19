d00e8f938b94e2ae38710b5468998d2502d5f329
CanelaLedezma <49193970@est.ort.edu.ar>
2026-06-19 11:52:43 -0300

    UNIQUE hecho
    Updated the 'name' column to be unique and modified the latitude and longitude values for provinces.

--- TP04/basededatos/bd_provinces.sql
+++ TP04/basededatos/bd_provinces.sql
@@ -1,13 +1,14 @@
 CREATE TABLE provinces (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(150) NOT NULL,
+    name VARCHAR(150) NOT NULL UNIQUE,
     full_name VARCHAR(250) NOT NULL,
     latitude NUMERIC(9,6) NOT NULL,
     longitude NUMERIC(9,6) NOT NULL,
     display_order INTEGER
 );
 
-INSERT INTO provinces (name, full_name, latitude, longitude, display_order)
-VALUES 
-('Buenos Aires', 'Provincia de Buenos Aires', -36.000000, -60.000000, 1),
-('Cordoba', 'Provincia de Cordoba', -31.400000, -64.100000, 2);
+INSERT INTO provinces
+(name, full_name, latitude, longitude, display_order)
+VALUES
+('Buenos Aires', 'Provincia de Buenos Aires', -36.676941, -60.558832, 1),
+('Cordoba', 'Provincia de Cordoba', -31.416668, -64.183334, 2);
